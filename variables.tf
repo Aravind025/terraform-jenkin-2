13dc34eb8ac317807932f41efcf69bde908bbe3f
Aravind025 <82218577+Aravind025@users.noreply.github.com>
2021-05-27 13:58:34 +0530

    Create variables.tf

---
+++ variables.tf
@@ -0,0 +1,9 @@
+variable "access_key" {
+  default = "AKIAZQF7PDA672KTRMXP"
+  }
+variable "secret_key" {
+  default = "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"
+  }
+variable "region" {
+  default = "us-east-2"
+  }
