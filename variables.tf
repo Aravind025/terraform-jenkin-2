1ce7424d19e3fe9e124114985a5a00847ae82a8a
Aravind025 <82218577+Aravind025@users.noreply.github.com>
2021-05-27 09:32:27 +0530

    Update variables.tf

--- variables.tf
+++ variables.tf
@@ -1,9 +1,3 @@
-variable "access_key" {
-  default = "AKIA5KOY4NEOEZ4VCKGU"
-  }
-variable "secret_key" {
-  default = "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"
-  }
 variable "region" {
   default = "us-east-2"
   }
